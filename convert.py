54928dcb0ddcd548ab95ae27da34ef1b40653f14
Dale Auten <dauten96@gmail.com>
2018-12-12 14:59:36 -0600

    added programs

---
+++ convert.py
@@ -0,0 +1,43 @@
+import sys
+import string
+import os
+
+if len(sys.argv) < 2:
+	print("Usage:\npython3 convert.py <text file>")
+
+filename = sys.argv[1]
+
+class line(object):
+	path = ""
+	md5 = ""
+	isUnique = True #assume there are no copies until proven otherwise
+
+	def __init__(self, p, h):
+		self.path = p
+		self.md5 = h
+
+
+
+file = open(filename, "rb")
+arr = []
+
+for l in file:
+    if len(l) > 48:
+        arr.append( line(l[99:], l[0:94]) )
+
+for a in arr:
+#    print(type(a.path))
+    a.path = str(a.path).replace("\\x00", "")[2:-5]
+    a.md5 = str(a.md5).replace("\\x00", "")[2:-1]
+    a.path = str(a.path).replace("\\\\", "/")
+    a.path = str(a.path).replace("\r", "")
+    a.path = str(a.path).replace("\n", "")
+    print(a.path)
+    print(a.md5)
+    master = ""
+    for folder in a.path.split("/")[:-1]:
+        master += folder + "/"
+        if not os.path.exists(master):
+            os.mkdir(master)
+
+    file = open(a.path+"_COPY.txt", "w+").write(a.md5)
---
+++ identify_dup.py
@@ -0,0 +1,30 @@
+import sys
+
+if len(sys.argv) < 2:
+	print("Usage:\npython3 identify_dup.py <text file>")
+
+filename = sys.argv[1]
+
+class line(object):
+	path = ""
+	md5 = ""
+	isUnique = True #assume there are no copies until proven otherwise
+
+	def __init__(self, p, h):
+		self.path = p
+		self.md5 = h
+
+
+
+file = open(filename, "rb")
+arr = []
+
+for l in file:
+	arr.append( line(l[94:], l[0:94]) )
+
+print("Hash,File A,File Two")
+for l in range(1, len(arr)-1):
+	if arr[l-1].md5 == arr[l].md5 and arr[l-1].path != arr[l].path:# and arr[l-1].path.split("\\")[-1] !=  arr[l].path.split("\\")[-1]:
+		print(str(arr[l-1].md5)+","+str(arr[l-1].path)[:-4].rstrip()+","+str(arr[l].path)[:-4].rstrip())
+		arr[l-1].isUnique = False
+		arr[l].isUnique = False
